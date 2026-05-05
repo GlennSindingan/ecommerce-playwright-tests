718a3788c2a0b40c3e193c611bf6af83669d67da
Glenn Sindingan <glennsindingan322@gmail.com>
2026-05-05 12:29:17 +0800

    Added account deletion steps

--- pages/header.py
+++ pages/header.py
@@ -5,3 +5,8 @@ class HeaderPage:
     def __init__(self, page: Page):
         self.page = page
 
+        # Header
+        self.delete_account_button = page.locator(".fa-trash-o")
+
+    def delete_account(self):
+        self.delete_account_button.click()
--- test/test_login_page.py
+++ test/test_login_page.py
@@ -8,18 +8,20 @@ from utils.test_data import my_address_data
 
 def test_register_and_delete_user(page: Page):
     login_page = LoginPage(page)
+    header_page = HeaderPage(page)
 
     page.goto(LOGIN_URL)
 
-    login_page.account_signup("gasdas125", "eztywetzsn@gmail.com")
+    login_page.account_signup("korikong322", "fineek322 @gmail.com")
     expect(page.get_by_role("heading", name="Enter Account Information")).to_be_visible()
     login_page.enter_account_info("cofee", "pass322421123")
     login_page.enter_address_info(my_address_data)
     expect(page.get_by_text("Account Created!")).to_be_visible()
     login_page.click_continue()
 
-    # TODO: add a account deletion flow
-    # TODO: add steps to the header file
+    header_page.delete_account()
+    expect(page.get_by_text("Account Deleted")).to_be_visible()
+
 
 
 
